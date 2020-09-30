a52720daaae56a96f230e819f38516305ab19c2d
Connor-B130 <71726102+Connor-B130@users.noreply.github.com>
2020-09-30 18:37:29 -0400

    After while loop was added

--- findAverage.py
+++ findAverage.py
@@ -1,5 +1,9 @@
+scoreCount = 0
 numberOfTests = int(input("Please enter the number of tests you want to average: "))
-score1 = float(input("Please enter a score: "))
-score2 = float(input("Please enter a score: "))
-average = (score1 + score2)/2
-print("The average is ",average)
+total = 0
+while scoreCount < numberOfTests:
+ score = int(input("Please enter a score: "))
+ scoreCount += 1
+ total = total + score
+average = total/scoreCount
+print("The average is ",average) 
